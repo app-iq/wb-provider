0eaf5dfa8c541f7f557b51ee6c729be74e705708
Ali Faris <alihcompiler@gmail.com>
2022-09-21 01:32:04 +0300

    fix buildRootReducer function

--- src/Data/Reducer.ts
+++ src/Data/Reducer.ts
@@ -4,8 +4,7 @@ export type Reducer<TState, TAction extends Action<unknown, unknown>> = (state: 
 
 export function buildRootReducer<TState>(reducers: Reducer<TState, Action<unknown, unknown>>[] = []): Reducer<TState, Action<unknown, unknown>> {
     return (state, action) => {
-        const allReducers: Reducer<TState, Action<unknown, unknown>>[] = reducers.concat(reducers);
-        return allReducers.reduce(((state, reduce) => {
+        return reducers.reduce(((state, reduce) => {
             const newState = reduce(state, action);
             if (newState === undefined) {
                 return state;
